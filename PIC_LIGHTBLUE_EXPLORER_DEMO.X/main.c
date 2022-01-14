78da1cc3c1bd44278e5151c90afd8565e72b3f33
Jenaro Garcia <jjegarcia@hotmail.com>
2022-01-14 08:05:15 +0000

    serial talk to transceiver '

--- PIC_LIGHTBLUE_EXPLORER_DEMO.X/main.c
+++ PIC_LIGHTBLUE_EXPLORER_DEMO.X/main.c
@@ -64,7 +64,7 @@
 static char statusBuffer[MAX_BUFFER_SIZE];      /**< Status Buffer instance passed to RN487X drive used for Asynchronous Message Handling (see *asyncBuffer in rn487x.c) */
 static char lightBlueSerial[MAX_BUFFER_SIZE];   /**< Message Buffer used for CDC Serial communication when connected. Terminated by \r, \n, MAX character Passes messages to BLE for transmisison. */
 static uint8_t serialIndex;                     /**< Local index value for serial communication buffer. */
-
+uint8_t value;
 /*
                          Main application
  */
@@ -85,52 +85,56 @@ int main(void)
 
     while (1)
     {
-        if (RN487X_IsConnected() == true)
-        {
-            if (TIMER_FLAG_SET() == true)
-            {
-                RESET_TIMER_INTERRUPT_FLAG;
-
-                LIGHTBLUE_TemperatureSensor();
-                LIGHTBLUE_AccelSensor();
-                LIGHTBLUE_PushButton();
-                LIGHTBLUE_LedState();
-                LIGHTBLUE_SendProtocolVersion();
-            }
-            else
-            {
-                while (RN487X_DataReady())
-                {
-                    LIGHTBLUE_ParseIncomingPacket(RN487X_Read());
-                }
-                while (uart[UART_CDC].DataReady())
-                {
-                    lightBlueSerial[serialIndex] = uart[UART_CDC].Read();
-                    if ((lightBlueSerial[serialIndex] == '\r')
-                        || (lightBlueSerial[serialIndex] == '\n')
-                        || (serialIndex == (sizeof(lightBlueSerial) - 1)))
-                    {
-                        lightBlueSerial[serialIndex] = '\0';
-                        LIGHTBLUE_SendSerialData(lightBlueSerial);
-                        serialIndex = 0;
-                    }
-                    else
-                    {
-                        serialIndex++;
-                    }
-                }
-                
-            }
-        }
-        else
+//        if (RN487X_IsConnected() == true)
+//        {
+//            if (TIMER_FLAG_SET() == true)
+//            {
+//                RESET_TIMER_INTERRUPT_FLAG;
+//
+//                LIGHTBLUE_TemperatureSensor();
+//                LIGHTBLUE_AccelSensor();
+//                LIGHTBLUE_PushButton();
+//                LIGHTBLUE_LedState();
+//                LIGHTBLUE_SendProtocolVersion();
+//            }
+//            else
+//            {
+//                while (RN487X_DataReady())
+//                {
+//                    LIGHTBLUE_ParseIncomingPacket(RN487X_Read());
+//                }
+//                while (uart[UART_CDC].DataReady())
+//                {
+//                    lightBlueSerial[serialIndex] = uart[UART_CDC].Read();
+//                    if ((lightBlueSerial[serialIndex] == '\r')
+//                        || (lightBlueSerial[serialIndex] == '\n')
+//                        || (serialIndex == (sizeof(lightBlueSerial) - 1)))
+//                    {
+//                        lightBlueSerial[serialIndex] = '\0';
+//                        LIGHTBLUE_SendSerialData(lightBlueSerial);
+//                        serialIndex = 0;
+//                    }
+//                    else
+//                    {
+//                        serialIndex++;
+//                    }
+//                }
+//                
+//            }
+//        }
+//        else
         {
             while(RN487X_DataReady())
             {
-                uart[UART_CDC].Write(RN487X_Read());
+                value =RN487X_Read();
+                lightBlueSerial[serialIndex]=value;
+                uart[UART_CDC].Write(value);
+                serialIndex++;
             }
             while (uart[UART_CDC].DataReady())
             {
-                RN487X.Write(uart[UART_CDC].Read());
+                value=uart[UART_CDC].Read();
+                RN487X.Write(value);
             }
         }
     }
