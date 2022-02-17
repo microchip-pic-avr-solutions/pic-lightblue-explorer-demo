1335827dcc143c578037009ff1c7fbfab162c4ef
Jenaro Garcia <jjegarcia@hotmail.com>
2022-02-17 15:53:49 +0000

    interface ready

--- PIC_LIGHTBLUE_EXPLORER_DEMO.X/main.c
+++ PIC_LIGHTBLUE_EXPLORER_DEMO.X/main.c
@@ -1,42 +1,4 @@
-/**
-  Generated Main Source File
-
-  Company:
-    Microchip Technology Inc.
-
-  File Name:
-    main.c
-
-  Summary:
-    This is the main file generated for use with PIC-Explorer
-
-  Description:
-    This header file provides implementations for driver APIs for all modules selected in the GUI.
- */
-
-/*
-    (c) 2018 Microchip Technology Inc. and its subsidiaries. 
-    
-    Subject to your compliance with these terms, you may use Microchip software and any 
-    derivatives exclusively with Microchip products. It is your responsibility to comply with third party 
-    license terms applicable to your use of third party software (including open source software) that 
-    may accompany Microchip software.
-    
-    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER 
-    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY 
-    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS 
-    FOR A PARTICULAR PURPOSE.
-    
-    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
-    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
-    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP 
-    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO 
-    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL 
-    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT 
-    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS 
-    SOFTWARE.
- */
-
+// Main File
 #include "mcc_generated_files/mcc.h"
 #include "mcc_generated_files/application/LIGHTBLUE_service.h"
 #include "mcc_generated_files/rn487x/rn487x_interface.h"
@@ -84,64 +46,15 @@ int main(void) {
     LIGHTBLUE_Initialize();
     serialIndex = 0;
     while (1) {
-        //        if (RN487X_IsConnected() == true)
-        //        {
-        //            if (TIMER_FLAG_SET() == true)
-        //            {
-        //                RESET_TIMER_INTERRUPT_FLAG;
-        //
-        //                LIGHTBLUE_TemperatureSensor();
-        //                LIGHTBLUE_AccelSensor();
-        //                LIGHTBLUE_PushButton();
-        //                LIGHTBLUE_LedState();
-        //                LIGHTBLUE_SendProtocolVersion();
-        //            }
-        //            else
-        //            {
-        //                while (RN487X_DataReady())
-        //                {
-        //                    LIGHTBLUE_ParseIncomingPacket(RN487X_Read());
-        //                }
-        //                while (uart[UART_CDC].DataReady())
-        //                {
-        //                    lightBlueSerial[serialIndex] = uart[UART_CDC].Read();
-        //                    if ((lightBlueSerial[serialIndex] == '\r')
-        //                        || (lightBlueSerial[serialIndex] == '\n')
-        //                        || (serialIndex == (sizeof(lightBlueSerial) - 1)))
-        //                    {
-        //                        lightBlueSerial[serialIndex] = '\0';
-        //                        LIGHTBLUE_SendSerialData(lightBlueSerial);
-        //                        serialIndex = 0;
-        //                    }
-        //                    else
-        //                    {
-        //                        serialIndex++;
-        //                    }
-        //                }
-        //                
-        //            }
-        //        }
-        //        else
-        //        {
-        while (RN487X_DataReady()) {
+         while (RN487X_DataReady()) {
             value == RN487X_Read();
-            lightBlueSerial[serialIndex] = value;
-            serialIndex++;
             uart[UART_CDC].Write(value);
         }
         while (uart[UART_CDC].DataReady()) {
             value = uart[UART_CDC].Read();
-            //                lightBlueSerial[serialIndex]=value;
-            //                serialIndex++;
-            //                uart[UART_CDC].Write(value);
-            
             RN487X.Write(value);
-
+            
         }
     }
-    //    }
     return 0;
 }
-/**
- End of File
- */
